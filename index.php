938aaed8f092df724480991745c3205faf18bc2a
Dhanang <ariadhanang@gmail.com>
2021-11-11 06:19:06 +0700

    add separate header and footer files

--- index.php
+++ index.php
@@ -1,11 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Arabica</title>
-</head>
-<body>
-	<h1>Arabica WordPress Theme</h1>
-</body>
-</html>
+
+<h1>Arabica WordPress Theme</h1>
