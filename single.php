05f0e01024b6ba4292a50b854b4a0fdcd8e5a64f
Dhanang <ariadhanang@gmail.com>
2021-11-11 23:10:39 +0700

    single template for post

---
+++ single.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<?php if (have_posts()) : ?>
+	<div class="posts single">
+	<?php while(have_posts()) : the_post(); ?>
+		<article class="post">
+			<h2>
+				<a href="<?php the_permalink(); ?>">
+					<?php the_title(); ?>
+				</a>
+			</h2>
+			<div class="thumbnail">
+				<?php the_post_thumbnail(); ?>
+			</div>
+			<div class="content">
+				<?php the_content("Read more..."); ?>
+			</div>
+		</article>
+	<?php endwhile; ?>
+	</div>
+<?php endif; ?>
+
+<?php get_footer(); ?>
