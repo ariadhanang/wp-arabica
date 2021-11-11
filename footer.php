ac9585f194d2a0fea84062be5e384288080a97c3
Dhanang <ariadhanang@gmail.com>
2021-11-11 23:02:54 +0700

    add footer

--- footer.php
+++ footer.php
@@ -1,5 +1,14 @@
 		</div> <!-- .container -->
 	</div> <!-- #site-content -->
+	
+	<footer id="site-footer">
+		<div class="container">
+			<p>
+				<?php bloginfo("name"); ?> &middot; <a href="#">Kopi Theme</a>
+			</p>
+		</div>
+	</footer>
+	
 	<?php wp_footer(); ?>
 </body>
 </html>
