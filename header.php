c882dc536df62b1b0968bb6b4f934d545e6f050d
Dhanang <ariadhanang@gmail.com>
2021-11-11 07:05:31 +0700

    add primary menu

--- header.php
+++ header.php
@@ -13,4 +13,7 @@
 			<h1 class="site-title"><?php bloginfo("name"); ?></h1>
 			<p class="site-description"><?php bloginfo("description"); ?></p>
 		</div>
+		<div class="menu">
+			<?php wp_nav_menu(array('theme_location' => 'header-menu'));?>
+		</div>
 	</header>
