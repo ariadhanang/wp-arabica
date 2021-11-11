55c198f229c56e6abbb6c9688276de15fb9ea025
Dhanang <ariadhanang@gmail.com>
2021-11-11 07:20:25 +0700

    fix permalink

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<?php while(have_posts()) : the_post(); ?>
 		<article class="post">
 			<h2>
-				<a href="<?php get_permalink(); ?>">
+				<a href="<?php the_permalink(); ?>">
 					<?php the_title(); ?>
 				</a>
 			</h2>
