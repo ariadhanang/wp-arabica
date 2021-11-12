7a7e5647dacec82cdb67c8357c9b14fbefbe27f9
Dhanang <ariadhanang@gmail.com>
2021-11-12 19:39:40 +0700

    adjust meta style

--- home.php
+++ home.php
@@ -19,7 +19,7 @@
 				</div>
 				<div class="meta">
 					<span class="time">
-						Posted at <?php the_time(); ?> • Category <?php the_category(","); ?>
+						Posted at <?php the_time(); ?> • Category <?php the_category(", "); ?>
 					</span>
 				</div>
 			</article>
