bf98c97bdedbcd609a6e647eb54c0cee13b75762
Dhanang <ariadhanang@gmail.com>
2021-11-12 19:28:37 +0700

    one column

--- home.php
+++ home.php
@@ -14,7 +14,7 @@
 					<?php the_post_thumbnail(); ?>
 				</div>
 				<div class="content">
-					<?php the_excerpt(); ?>
+					<?php the_content("Read more..."); ?>
 				</div>
 			</article>
 		<?php endwhile; ?>
